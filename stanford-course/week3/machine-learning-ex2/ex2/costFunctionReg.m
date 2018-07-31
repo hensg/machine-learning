3077d082d69ff47c62c168b3284c5a1ea19ccd3b
Henrique Goulart <henriquedsg89@gmail.com>
2018-07-30 22:34:01 -0300

    Logistic regression cost function, sigmoid and gradient

--- stanford-course/week3/machine-learning-ex2/ex2/costFunctionReg.m
+++ stanford-course/week3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -16,10 +16,16 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+h = sigmoid(X*theta);
 
+J = 1/m * (-y' * log(h) - (1-y)' * log(1-h));
 
+sqrTheta = theta(2:end,:) .^ 2;
 
+J = J + (lambda * sum(sqrTheta) / (2*m));
 
+grad = grad + 1/m * X' * (h - y);
+grad(2:end,:) = grad(2:end,:) + ((lambda/m) * theta(2:end, :));
 
 
 % =============================================================
